4f09b3687815c8df2a3c7b6953b1b9f5c01ea8cb
root <risul.islam@regfire.com>
2015-11-11 22:30:07 +0100

    Updated to At.js 1.4.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: 'risul:autocomplete',
     summary: 'Meteor package for At.js - Add Github like mentions autocomplete to your application',
-    version: '1.3.2',
+    version: '1.4.0',
     git: 'https://github.com/risul/meteor-autocomplete'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@1.0');
     api.use("jquery", "client");
-    api.use("risul:caret@0.0.2", "client")
+    api.use("risul:caret@0.2.2", "client")
     api.addFiles(['lib/jquery.atwho.css', 'lib/jquery.atwho.js'], ['client']);
 });
